6511d625ce3a8245ddbbe0f258ef0367bf181ed3
vitaleymulikacatchup <vitaley.mulika.catchup@gmail.com>
2025-10-17 15:08:49 +0300

    Update project files

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-1': '#000',
+        'custom-2': '#fff',
+        'custom-3': '#6d33fc',
+        'custom-4': '#3b82f6',
+        'custom-5': '#000000',
+        'custom-6': '#ffffff'
+      },
+      fontFamily: {
+        'custom-1': ['system-ui', 'sans-serif']
+      }
+    }
+  }
+};
